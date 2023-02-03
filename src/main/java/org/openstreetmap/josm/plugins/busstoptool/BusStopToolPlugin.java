eb6ff8f0776ff93469e9b606176a744e46a4b619
Paweł Raszuk <pawel.raszuk@gmail.com>
2023-02-03 12:42:48 +0100

    Add init project/gradle files

---
+++ src/main/java/org/openstreetmap/josm/plugins/busstoptool/BusStopToolPlugin.java
@@ -0,0 +1,10 @@
+package org.openstreetmap.josm.plugins.busstoptool;
+
+import org.openstreetmap.josm.plugins.Plugin;
+import org.openstreetmap.josm.plugins.PluginInformation;
+
+public class BusStopToolPlugin extends Plugin {
+    public BusStopToolPlugin(PluginInformation info) {
+        super(info);
+    }
+}
